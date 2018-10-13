a339429ec79adc562ea0f4a73fd03ec4b935e9c2
sonu-goud <suresh.bt2932@gmail.com>
2018-10-13 19:55:58 +0530

    collab1 adding 2 lines in a java file

--- src/main/java/pack1/App.java
+++ src/main/java/pack1/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       
+    	System.out.println("adding 2 lines by collab1");
+    	System.out.println( "Hello World!" );
+       
     }
 }
