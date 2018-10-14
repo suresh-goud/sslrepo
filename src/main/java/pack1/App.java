0e20929f33f076d1d3b90b442e018f17153b6fea
suresh-goud <44023361+suresh-goud@users.noreply.github.com>
2018-10-14 21:48:02 +0530

    Update App.java

--- src/main/java/pack1/App.java
+++ src/main/java/pack1/App.java
@@ -8,7 +8,5 @@ public class App
 {
     public static void main( String[] args )
     {
-       
-    	System.out.println("adding 2 lines by collab1");
-    	System.out.println( "Hello World!" );
-}}
+    }
+}
